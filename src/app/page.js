405e228afca30dc61ede46e3ec6477997c1a397c
Nick <rsnick06@yahoo.com>
2025-04-02 23:51:58 -0700

    connection

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
     }
 
     const fetchData = () => {
-      fetch("http://localhost:5050/api/profiles", {
+      fetch("https://backend-scraper-qkcr.onrender.com/api/profiles", {
         headers: {
           Authorization: "Bearer " + process.env.NEXT_PUBLIC_API_SECRET_TOKEN
         }
